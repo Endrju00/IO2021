f4b999115298d34b6aa02fc4fc3758b02e0fd23d
Andrzej Kapczyński <75850182+Endrju00@users.noreply.github.com>
2021-11-03 11:02:13 +0100

    Update 2

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Main myObj = new Main();
         System.out.println(myObj.x);
+        System.out.println(myObj.y);
         // System.out.println('ZMIANA GIT');
     }
 }
